084488ec405930ad86db17238fbf3f5503791507
kunani1 <131540652+kunani1@users.noreply.github.com>
2023-09-01 10:24:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+BOT_TOKEN = "1912844190:AAEsu6K7yPJqoehmiX_6N12fIgyqJ_OB0ho"
+API_ID = "2929027"
+API_HASH = "2beecc3ee357e6e3f2b2e783d4159f9f"
+LOG_CHANNEL = "-1001856739396"
+AUTH_USERS = "959184369"
+DB_URL = "mongodb+srv://ksai:ksai@cluster0.kayzwoi.mongodb.net/cluster0?retryWrites=true&w=majority"
+DB_NAME = "BroadcastBot"
+BROADCAST_AS_COPY = "True"
